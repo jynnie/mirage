f182813816b90a3b85dcbedc0ae03ebddecbc168
jynnie <jynnie@mit.edu>
2018-02-23 17:13:00 -0500

    backup in progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var pump = require('pump');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
 
 gulp.task('img', function (cb) {
 	pump([
@@ -12,9 +13,15 @@ gulp.task('img', function (cb) {
 });
 
 gulp.task('sass', function(){
-	return gulp.src('src/sass/**/*.scss')
+	return gulp.src('src/scss/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('pug', function buildHTML() {
+	return gulp.src('src/*.pug')
+	  .pipe(pug())
+		.pipe(gulp.dest('dist'))
 });
 
 gulp.task('html', function (cb) {
@@ -26,7 +33,7 @@ gulp.task('html', function (cb) {
 	);
 });
 
-gulp.task('build', ['img', 'html', 'sass'], function (cb) {
+gulp.task('build', ['img', 'html', 'sass', 'pug'], function (cb) {
 	pump([
 		gulp.src('src/lib/*.js'),
 		gulp.dest('dist/js')
@@ -36,9 +43,10 @@ gulp.task('build', ['img', 'html', 'sass'], function (cb) {
 });
 
 gulp.task('stream', ['build'], function (cb) {
-	gulp.watch('src/sass/**/*.scss', ['build']);
+	gulp.watch('src/scss/**/*.scss', ['build']);
 	gulp.watch('src/lib/*.js', ['build']);
 	gulp.watch('src/*.html', ['build']);
+	gulp.watch('src/*.pug', ['build']);
 });
 
 gulp.task('default', ['stream']);
